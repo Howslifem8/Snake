38ec5cef03cab6cd5935c2c62b0dced38c9d2336
Howslifem8 <77900256+Howslifem8@users.noreply.github.com>
2025-07-03 17:14:41 -0700

    Added Snake head, Snake Food, and a randomiser function to randomly place food anywhere.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+//Game board
 var cellSize = 25;
 var rows = 27;
 var cols = 27;
 var canvas;
 var ctx;
+
+//Snake head 
+var snakeX = cellSize * 5;
+var snakeY = cellSize * 5;
+
+//Snake food 
+var foodX;
+var foodY;
+
 
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
@@ -10,10 +20,23 @@ window.onload = function() {
   canvas.width = cols * cellSize;
   ctx = canvas.getContext("2d");
 
+
+  placeFood();
   update(); 
 }
 
 function update() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "lime";
+  ctx.fillRect(snakeX, snakeY, cellSize, cellSize);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(foodX, foodY, cellSize, cellSize);
+}
+
+function placeFood() {
+  foodX = Math.floor(Math.random() * cols) * cellSize;
+  foodY = Math.floor(Math.random() * rows) * cellSize;
 }
